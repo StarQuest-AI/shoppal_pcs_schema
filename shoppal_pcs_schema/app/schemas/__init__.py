18f4a4a7bb20d74bd3691185bc82a72c9771f38c
hezhe <hezhe@shoppal.ai>
2024-04-10 15:49:07 +0800

    fix: __init__.py bug

--- shoppal_pcs_schema/app/schemas/__init__.py
+++
@@ -1,7 +0,0 @@
-from app.schemas.common import *
-from app.schemas.common_search_result import *
-from app.schemas.product_detail import *
-from app.schemas.product_search_result import *
-from app.schemas.search_request import *
-from app.schemas.video_detail import *
-from app.schemas.video_search_result import *
