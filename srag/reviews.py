ab6b246c3b43c3686769555c2b564e0ecd71365f
师奇 <148878129+shoppal-shiqi@users.noreply.github.com>
2024-05-14 10:33:25 +0800

    add srag schema (#13)
    Co-authored-by: shiqi <shiqi@shiqideMBP.lan>

---
+++ srag/reviews.py
@@ -0,0 +1,32 @@
+"""
+product reviews by e-commerce platform and third party review sites
+"""
+
+from typing import Optional
+
+from pydantic import BaseModel
+from shoppal_pcs_schema.schemas.product_detail import ReviewInfo, ProductPlatForm
+
+class EcommercePlatformReview(BaseModel):
+    """
+    e-commerce platform review model
+    """
+    platform: str = ProductPlatForm.AMAZON.value
+    review: ReviewInfo
+
+class ThirdPartyReview(BaseModel):
+    """
+    third party review model
+    """
+
+    url: str
+    full_content: str
+    trunk_content: Optional[list[str]] = None
+    product_ids: Optional[list[str]] = None
+
+
+class YoutubeReviews(ThirdPartyReview):
+    """
+    youtube reviews
+    """
+    
