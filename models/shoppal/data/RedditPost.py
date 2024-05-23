a47be9d89c0073fcff1c88ce02ea5c179395f13f
zhuguangbin <zhuguangbin@users.noreply.github.com>
2024-05-23 15:42:38 +0000

    Auto-generate Pydantic models from AVSC

---
+++ models/shoppal/data/RedditPost.py
@@ -0,0 +1,21 @@
+
+from datetime import date, datetime, time
+from decimal import Decimal
+from enum import Enum
+from typing import List, Optional, Dict, Union
+from uuid import UUID
+
+from pydantic import BaseModel, Field
+
+
+class RedditPost(BaseModel):
+    id: str
+    type_name: str
+    oid: str
+    uid: str
+    url: str
+    upvotes_count: str
+    comments_count: str
+    creation_time: str
+    json_data: str
+    crawl_time: str
