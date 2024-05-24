2d760711646e0e30b8a1e3884622c269106f2bf6
zhuguangbin <zhuguangbin@shoppal.ai>
2024-05-24 15:03:53 +0800

    17 support avro and shoppal related data schema (#21)

---
+++ models/shoppal/data/QuoraPost.py
@@ -0,0 +1,26 @@
+
+from datetime import date, datetime, time
+from decimal import Decimal
+from enum import Enum
+from typing import List, Optional, Dict, Union
+from uuid import UUID
+
+from pydantic import BaseModel, Field
+
+
+class QuoraPost(BaseModel):
+    id: str
+    type_name: str
+    id_str: str
+    oid: str
+    uid: str
+    url: str
+    views_count: str
+    upvotes_count: str
+    shares_count: str
+    comments_count: str
+    creation_time: str
+    updated_time: str
+    cursor: str
+    json_raw: str
+    crawl_time: str
