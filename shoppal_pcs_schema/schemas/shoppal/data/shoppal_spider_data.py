c8c5863de46067273ae869d8137c028260fbfd45
zhuguangbin <zhuguangbin@shoppal.ai>
2024-06-04 15:01:18 +0800

    add spider related schema (#40)

---
+++ shoppal_pcs_schema/schemas/shoppal/data/shoppal_spider_data.py
@@ -0,0 +1,18 @@
+
+from datetime import datetime
+from typing import Optional
+from uuid import UUID
+
+from pydantic import BaseModel, Field
+from shoppal_pcs_schema.schemas.shoppal.data.crawler_result import CrawlerResult
+
+class ShoppalSpiderData(BaseModel):
+    id: str # UUID1
+    url: str # URL
+    spider: Optional[str] = None # used for spider
+    source: Optional[int] = 0 # 数据来源，0：未知，1：Spider，2: DataService
+    crawl_result: Optional[CrawlerResult] # 爬虫结果
+    crawl_result_md5: Optional[str] # 爬虫结果MD5
+    crawl_time: datetime # 爬虫写入时间
+    update_time: datetime # 数据更新时间
+    
