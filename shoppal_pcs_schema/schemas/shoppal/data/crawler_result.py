7c45b3f759dad0996c94b06a48e870f6b44bd26d
zhuguangbin <zhuguangbin@users.noreply.github.com>
2024-06-05 08:56:19 +0000

    Auto-generate Pydantic models from AVSC

--- shoppal_pcs_schema/schemas/shoppal/data/crawler_result.py
+++ shoppal_pcs_schema/schemas/shoppal/data/crawler_result.py
@@ -14,6 +14,33 @@ class CrawlerRequestInfo(BaseModel):
     api_params: str
 
 
+class ParsedWebPageContent(BaseModel):
+    title: Optional[str]
+    author: Optional[str]
+    hostname: Optional[str]
+    date: Optional[str]
+    fingerprint: Optional[str]
+    id: Optional[str]
+    license: Optional[str]
+    comments: Optional[str]
+    raw_text: Optional[str]
+    text: Optional[str]
+    language: Optional[str]
+    image: Optional[str]
+    pagetype: Optional[str]
+    filedate: Optional[str]
+    source: Optional[str]
+    source_hostname: Optional[str]
+    excerpt: Optional[str]
+    categories: Optional[str]
+    tags: Optional[str]
+
+
+class WebPageContent(BaseModel):
+    raw_html: Optional[str]
+    parsed_webpage_content: Optional[ParsedWebPageContent]
+
+
 class Content(BaseModel):
     title: Optional[str]
     author: Optional[str]
@@ -42,6 +69,7 @@ class CrawlerResult(BaseModel):
     pc_status: int = Field(200, ge=-2**31, le=(2**31 - 1))
     original_url: str
     url: str
+    webpage: Optional[WebPageContent]
     html: str = ""
     content: Optional[Content]
     doc_filter_result: List[str]
