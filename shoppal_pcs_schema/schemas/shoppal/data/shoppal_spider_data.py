896d7379e9d4a4925284d1e56b1e46d3a8462361
zhuguangbin <zhuguangbin@shoppal.ai>
2024-06-05 16:56:01 +0800

    refactor shoppal_spider_data schema, support different content_types

--- shoppal_pcs_schema/schemas/shoppal/data/shoppal_spider_data.py
+++ shoppal_pcs_schema/schemas/shoppal/data/shoppal_spider_data.py
@@ -1,16 +1,25 @@
 
 from datetime import datetime
-from typing import Optional
+from typing import Optional, Enum
 from uuid import UUID
 
 from pydantic import BaseModel, Field
 from shoppal_pcs_schema.schemas.shoppal.data.crawler_result import CrawlerResult
 
+class ContentType(Enum):
+    OTHER = "other"
+    WEBPAGE = "webpage"
+    SUBTITLE = "subtitle"
+    AUDIO = "audio"
+    VIDEO = "video"
+    IMAGE = "image"
+
 class ShoppalSpiderData(BaseModel):
     id: str # UUID1
     url: str # URL
-    spider: Optional[str] = None # used for spider
+    content_type: Optional[ContentType] = ContentType.OTHER # used for spider
     source: Optional[int] = 0 # 数据来源，0：未知，1：Spider，2: DataService
+    spider: Optional[str] # 爬虫名称, 由不同的content_type交给不同的爬虫处理解析
     crawl_result: Optional[CrawlerResult] # 爬虫结果
     crawl_result_md5: Optional[str] # 爬虫结果MD5
     crawl_time: Optional[datetime] # 爬虫写入时间
