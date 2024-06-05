a1aa1e79903288122c4f24def2ac57b7c9fabc6b
zhuguangbin <zhuguangbin@users.noreply.github.com>
2024-06-05 15:48:40 +0000

    Auto-generate Pydantic models from AVSC

--- shoppal_pcs_schema/schemas/shoppal/data/crawler_result.py
+++ shoppal_pcs_schema/schemas/shoppal/data/crawler_result.py
@@ -11,44 +11,44 @@ from pydantic import BaseModel, Field
 class CrawlerRequestInfo(BaseModel):
     use_api: bool = True
     api_base: str = "https://api.scrapfly.io/scrape"
-    api_params: str
+    api_params: Optional[str] = None
 
 
 class ParsedWebPageContent(BaseModel):
-    title: Optional[str]
-    author: Optional[str]
-    hostname: Optional[str]
-    date: Optional[str]
-    fingerprint: Optional[str]
-    id: Optional[str]
-    license: Optional[str]
-    comments: Optional[str]
-    raw_text: Optional[str]
-    text: Optional[str]
-    language: Optional[str]
-    image: Optional[str]
-    pagetype: Optional[str]
-    filedate: Optional[str]
-    source: Optional[str]
-    source_hostname: Optional[str]
-    excerpt: Optional[str]
-    categories: Optional[str]
-    tags: Optional[str]
+    title: Optional[str] = None
+    author: Optional[str] = None
+    hostname: Optional[str] = None
+    date: Optional[str] = None
+    fingerprint: Optional[str] = None
+    id: Optional[str] = None
+    license: Optional[str] = None
+    comments: Optional[str] = None
+    raw_text: Optional[str] = None
+    text: Optional[str] = None
+    language: Optional[str] = None
+    image: Optional[str] = None
+    pagetype: Optional[str] = None
+    filedate: Optional[str] = None
+    source: Optional[str] = None
+    source_hostname: Optional[str] = None
+    excerpt: Optional[str] = None
+    categories: Optional[str] = None
+    tags: Optional[str] = None
 
 
 class WebPageContent(BaseModel):
-    raw_html: Optional[str]
-    parsed_webpage_content: Optional[ParsedWebPageContent]
+    raw_html: Optional[str] = None
+    parsed_webpage_content: Optional[ParsedWebPageContent] = None
 
 
 class SubTitleTrunk(BaseModel):
-    trunck_content_str: Optional[str]
-    trunck_content_position: Optional[int]
+    trunck_content_str: Optional[str] = None
+    trunck_content_position: Optional[int] = None
 
 
 class SubtitleContent(BaseModel):
-    concatenated_string: Optional[str]
-    subtitle_trunks: Optional[List[SubTitleTrunk]]
+    concatenated_string: Optional[str] = None
+    subtitle_trunks: Optional[List[SubTitleTrunk]] = None
 
 
 class CrawlerResult(BaseModel):
@@ -58,5 +58,5 @@ class CrawlerResult(BaseModel):
     original_url: str
     url: str
     webpage: Optional[WebPageContent]
-    subtitle: Optional[SubtitleContent]
+    subtitle: Optional[SubtitleContent] = None
     doc_filter_result: List[str]
