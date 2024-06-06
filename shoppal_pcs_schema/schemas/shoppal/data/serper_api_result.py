d6918918bf33b5d53313e24a3b3cf306cbde1b7c
lxkaka0736 <lxkaka0736@users.noreply.github.com>
2024-06-06 09:32:27 +0000

    Auto-generate Pydantic models from AVSC

--- shoppal_pcs_schema/schemas/shoppal/data/serper_api_result.py
+++ shoppal_pcs_schema/schemas/shoppal/data/serper_api_result.py
@@ -1,6 +1,11 @@
-from typing import List, Optional
 
-from pydantic import BaseModel
+from datetime import date, datetime, time
+from decimal import Decimal
+from enum import Enum
+from typing import List, Optional, Dict, Union
+from uuid import UUID
+
+from pydantic import BaseModel, Field
 
 
 class SearchMetadata(BaseModel):
