42d09626f69bcbfcc9fa7deb18bff8c13e021bda
wanglinxiao <wanglinxiao@shoppal.ai>
2024-06-06 17:32:06 +0800

    fix avsc

--- shoppal_pcs_schema/schemas/shoppal/data/serper_api_result.py
+++ shoppal_pcs_schema/schemas/shoppal/data/serper_api_result.py
@@ -1,11 +1,6 @@
+from typing import List, Optional
 
-from datetime import date, datetime, time
-from decimal import Decimal
-from enum import Enum
-from typing import List, Optional, Dict, Union
-from uuid import UUID
-
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 
 class SearchMetadata(BaseModel):
@@ -98,25 +93,12 @@ class Pagination(BaseModel):
     next: str
 
 
-class OtherPages(BaseModel):
-    1: str
-    3: str
-    4: str
-    5: str
-    6: str
-    7: str
-    8: str
-    9: str
-    10: str
-
-
 class SerpapiPagination(BaseModel):
     current: int
     previous_link: str
     previous: str
     next_link: str
     next: str
-    other_pages: OtherPages
 
 
 class SerperAPiResult(BaseModel):
