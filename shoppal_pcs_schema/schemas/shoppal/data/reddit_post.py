c7516869a926b87c9a8aeeab5b3570a31230783f
zhuguangbin <zhuguangbin@users.noreply.github.com>
2024-06-06 10:11:08 +0000

    Auto-generate Pydantic models from AVSC

--- shoppal_pcs_schema/schemas/shoppal/data/reddit_post.py
+++ shoppal_pcs_schema/schemas/shoppal/data/reddit_post.py
@@ -1,7 +1,8 @@
+
 from datetime import date, datetime, time
 from decimal import Decimal
 from enum import Enum
-from typing import Dict, List, Optional, Union
+from typing import List, Optional, Dict, Union
 from uuid import UUID
 
 from pydantic import BaseModel, Field
