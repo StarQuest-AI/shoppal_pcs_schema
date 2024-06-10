c83ae7e4bd3e6d9ceb9df0c3216c32ce6c4cbe1e
师奇 <148878129+shoppal-shiqi@users.noreply.github.com>
2024-06-10 12:21:13 +0800

    58 srag response (#61)
    * add CitationResponse for srag and code format
    
    * Applied automatic fixes from linters
    
    * add __init__.py
    
    * Applied automatic fixes from linters
    
    * add srag_common.py
    
    * add srag_common.py
    
    * Applied automatic fixes from linters
    
    * add favicon.py
    
    * Applied automatic fixes from linters
    
    * add favicon.py
    
    * Applied automatic fixes from linters
    
    ---------
    
    Co-authored-by: shiqi <shiqi@shiqideMacBook-Pro.local>
    Co-authored-by: shoppal-shiqi <shoppal-shiqi@users.noreply.github.com>

--- shoppal_pcs_schema/schemas/__init__.py
+++ shoppal_pcs_schema/schemas/__init__.py
@@ -1,5 +1,6 @@
 from shoppal_pcs_schema.schemas.common import *
 from shoppal_pcs_schema.schemas.common_search_result import *
+from shoppal_pcs_schema.schemas.favicon import *
 from shoppal_pcs_schema.schemas.product_detail import *
 from shoppal_pcs_schema.schemas.product_search_result import *
 from shoppal_pcs_schema.schemas.search_request import *
