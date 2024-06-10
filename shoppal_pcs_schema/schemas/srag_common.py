b753c9bd86b0852efa8b664c7287692975e6e788
shiqi <shiqi@shiqideMacBook-Pro.local>
2024-06-10 12:40:14 +0800

    add favicon.py

--- shoppal_pcs_schema/schemas/srag_common.py
+++ shoppal_pcs_schema/schemas/srag_common.py
@@ -1,11 +1,11 @@
 """
 srag common pydantic
 """
-
+from typing import Optional
 from shoppal_pcs_schema.schemas.common_search_result import GoogleSearchResult
 
 
 class CitationResponse(GoogleSearchResult):
     """citation response"""
 
-    cover_images: list[str]
+    cover_images: Optional[list[str]]
