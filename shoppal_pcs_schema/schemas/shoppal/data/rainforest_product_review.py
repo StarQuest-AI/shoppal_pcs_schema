40d61e061016c9c52214379b972993038eb1bc69
wanglinxiao <wanglinxiao@shoppal.ai>
2024-06-19 17:30:11 +0800

    review data schema

--- shoppal_pcs_schema/schemas/shoppal/data/rainforest_product_review.py
+++ shoppal_pcs_schema/schemas/shoppal/data/rainforest_product_review.py
@@ -4,8 +4,10 @@ from decimal import Decimal
 from enum import Enum
 from typing import List, Optional, Dict, Union
 from uuid import UUID
+import uuid
 
 from pydantic import BaseModel, Field
+from shoppal_pcs_schema.schemas.shoppal.data.common import Source
 
 
 class RequestInfo(BaseModel):
@@ -134,3 +136,20 @@ class RainforestProductReview(BaseModel):
     summary: Optional[Summary] = None
     reviews: Optional[List[Reviews]] = None
     pagination: Optional[Pagination] = None
+
+
+class ShoppalSpiderProductReviewData(BaseModel):
+    id: str = uuid.uuid1().hex  # UUID1
+    url: str  # URL
+    host: str  # 商品所在网站的域名，比如：www.amazon.com
+    product_id: Optional[str] = None  # 商品ID，比如：Amazon为ASIN: B0CBLKS51N
+    product_title: Optional[str] = None  # 商品Title
+    parser_name: Optional[str] = None  # used for spider
+    source: Optional[Source] = 0  # 数据来源
+    raw_result: Optional[str] = (
+        None  # 原始抓取结果，如果为Rainforest则为API则为Response的json结果，如果为自研爬虫则为原始html
+    )
+    crawl_result: Optional[RainforestProductReview] = None  # 商品review解析结果，存储为JSON格式，遵循RainForest的数据模型
+    create_time: Optional[datetime] = datetime.now()  # 爬虫写入时间
+    update_time: Optional[datetime] = datetime.now()  # 数据更新时间
+    task_id: Optional[int] = None  # 爬虫任务ID
