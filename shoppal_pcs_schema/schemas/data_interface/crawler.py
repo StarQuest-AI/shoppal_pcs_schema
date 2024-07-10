310a2c42172d6c2ea5db67121c5f1f4f9bcd067c
lxkaka0736 <168156776+lxkaka0736@users.noreply.github.com>
2024-07-10 16:48:07 +0800

    Update (#107)

--- shoppal_pcs_schema/schemas/data_interface/crawler.py
+++ shoppal_pcs_schema/schemas/data_interface/crawler.py
@@ -1,7 +1,8 @@
+from decimal import Decimal
 from enum import Enum
-from typing import Optional
+from typing import List, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict, Field
 
 from shoppal_pcs_schema.schemas.shoppal.data.rainforest_product_review import \
     ShoppalSpiderProductReviewData
@@ -53,3 +54,34 @@ class CrawlerProductReviewDataResponse(BaseModel):
 
     status: CrawlerStatus
     data: Optional[ShoppalSpiderProductReviewData] = None
+
+
+class MainImage(BaseModel):
+    """
+    Main image schema.
+    """
+
+    link: Optional[str] = None
+
+
+class ProductCard(BaseModel):
+    """
+    Product card schema.
+    """
+
+    product_id: str = Field(alias="asin")
+    title: str
+    main_image: Optional[MainImage] = None
+    images: Optional[List[MainImage]] = None
+    link: Optional[str] = None
+    origin_price: Optional[Decimal] = None
+    current_price: Optional[Decimal] = None
+    currency_symbol: Optional[str] = None
+    discount: Optional[Decimal] = None
+    rating: Optional[Decimal] = None
+    ratings_total: Optional[int] = None
+    sell_amount_last_month: Optional[int] = None
+    brand: Optional[str] = None
+    categories: Optional[List] = None
+
+    model_config = ConfigDict(populate_by_name=True)
